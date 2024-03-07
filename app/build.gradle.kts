f14f3597873d657413e720a51c2208259c0567b5
Alexey Filimonov <filimonov.alks@gmail.com>
2024-03-07 23:16:38 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,132 @@
+import module.AppModuleConfig
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    val moduleConfig = AppModuleConfig
+    namespace = moduleConfig.namespace
+    compileSdk = moduleConfig.compileSdk
+
+    defaultConfig {
+        applicationId = moduleConfig.namespace
+        minSdk = moduleConfig.minSdk
+        targetSdk = moduleConfig.targetSdk
+        versionCode = moduleConfig.versionCode
+        versionName = moduleConfig.versionName
+
+        testInstrumentationRunner = moduleConfig.testInstrumentationRunner
+    }
+
+    buildTypes {
+        debug {
+            isDebuggable = true
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+
+    compileOptions {
+        sourceCompatibility = moduleConfig.targetJdk
+        targetCompatibility = moduleConfig.targetJdk
+    }
+
+    kotlinOptions {
+        jvmTarget = moduleConfig.targetJdk.majorVersion
+    }
+}
+
+dependencies {
+
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.constraintlayout.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.ui.util)
+    implementation(libs.androidx.compose.ui.viewbinding)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.animation)
+    implementation(libs.androidx.compose.material.iconsExtended)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.androidx.lifecycle.viewmodel.savedstate)
+
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.preference)
+    implementation(libs.androidx.work.runtime)
+
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(libs.timber)
+    implementation(libs.material)
+
+    kapt(libs.hilt.ext.compiler)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation)
+    implementation(libs.hilt.workmanager)
+
+    kaptTest(libs.hilt.compiler)
+
+    kaptAndroidTest(libs.hilt.android.testing)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.androidx.room.testing)
+
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.test.rules)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit.ktx)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.hilt.android.testing)
+    androidTestImplementation(libs.conditionwatcher)
+    androidTestImplementation(libs.androidx.work.testing)
+}
---
+++ buildSrc/src/main/kotlin/module/AppModuleConfig.kt
@@ -0,0 +1,8 @@
+package module
+
+object AppModuleConfig : BaseModuleConfig() {
+    override val namespace = "ru.filimonov.hpa"
+    override val versionCode = 1
+    override val versionName = "1.0.0"
+//    override val testInstrumentationRunner: String = "ru.filimonov.hpa.AndroidJUnitRunnerWithHilt"
+}
